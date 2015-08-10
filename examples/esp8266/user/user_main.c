83b856071c9c121a69d6097c94a17b9dfafe5638
Andy Gelme <andyg@geekscape.org>
2015-08-10 15:33:17 +1000

    Add ESP8266 user_main.c source file header.

--- examples/esp8266/user/user_main.c
+++ examples/esp8266/user/user_main.c
@@ -1,4 +1,15 @@
 /*
+ * Please do not remove the following notices.
+ *
+ * \file       user_main.c
+ * \author     Andy Gelme <andyg@geekscape.org>
+ * \copyright  (c) 2015 by Geekscape Pty. Ltd.
+ * \license    AGPLv3 http://geekscape.org/static/aiko_license.html
+ *
+ * Description
+ * ~~~~~~~~~~~
+ * Aiko engine: ESP8266 implementation
+ *
  * To Do
  * ~~~~~
  * - None, yet.
